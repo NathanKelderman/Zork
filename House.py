9aa402c447b51981f8317eae90f1f8c072dd728c
rupertt <turnbulr@mail.gvsu.edu>
2017-11-13 13:52:06 -0500

    updated House file

--- House.py
+++ House.py
@@ -1,4 +1,14 @@
-class House:
-	def __init__ (self):
-		self.number_of_monsters = 6
+from Monster import Monster
+from random import randint
 
+class House:
+
+	def __init__ (self, numMonsters):
+		self.numMonsters = numMonsters
+		monstersArray = []
+
+	def addMonster (Monster):
+		monstersArray.append(Monster)
+
+	def getMonsters():
+		return monsterArray
